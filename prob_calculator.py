158342180b2ed9a2eec97438618fa2568157cc7f
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-15 13:15:25 +0800

    Add main hat class

---
+++ prob_calculator.py
@@ -0,0 +1,9 @@
+import copy
+import random
+
+class Hat:
+    """
+    Takes multiple arguments: color of the balls and the number of the balls of
+    a particular color.
+    Consist draw method that removes given balls at random
+    """
