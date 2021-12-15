c635aeaec67e27871bf7bdc65a544f3fe59e7db7
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-15 14:08:55 +0800

    Add init method to the hat class

--- prob_calculator.py
+++ prob_calculator.py
@@ -7,3 +7,14 @@ class Hat:
     a particular color.
     Consist draw method that removes given balls at random
     """
+
+    def __init__(self, **kwargs):
+        """
+        Takes multiple arguments with keys and values and transform them into a 
+        list with multiple string of keys.
+        The number of repeating keys depends on the value given 
+        """
+        self.contents = []
+        for key, value in kwargs.items():
+            for i in range(value):
+                self.contents.append(key)
